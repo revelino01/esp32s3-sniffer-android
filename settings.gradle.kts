4ce37fa23a3818b97ebd7979cfb7a44488cd2bb6
Revelino Adli <revelino01@users.noreply.github.com>
2026-05-31 13:38:10 +0700

    Add JitPack repository for usb-serial-for-android

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 rootProject.name = "ESP32-S3 Sniffer"
